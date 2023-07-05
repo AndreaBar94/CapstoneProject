062e6c2ccdabef0830a037288c63670da81bd29b
AndreaBar94 <andreabarocchi@gmail.com>
2023-07-05 16:14:35 +0200

    added icons for buttons in article, homepage, searchbar, profile

--- board_game_blog/src/components/PageNavbar.jsx
+++ board_game_blog/src/components/PageNavbar.jsx
@@ -67,11 +67,6 @@ const PageNavbar = () => {
                 Home
               </Link>
               <Form className="d-flex align-items-center mx-5" onSubmit={handleSearchSubmit}>
-                <span className="me-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                </svg>
-                </span>
                 <Form.Select className="me-2" value={selectedFilter} onChange={handleFilterChange}>
                   {filterOptions.map((filter) => (
                     <option key={filter} value={filter}>
@@ -82,13 +77,16 @@ const PageNavbar = () => {
                 <Form.Control
                   className="me-2"
                   type="search"
-                  placeholder="Search"
+                  placeholder="Type here..."
                   aria-label="Search"
                   value={searchKeyword}
                   onChange={handleSearchInputChange}
                   />
-                <Button variant="dark" type="submit">
+                <Button variant="dark" type="submit" className='d-flex align-items-center'>
                   Search
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search ms-2" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                  </svg>
                 </Button>
               </Form>
             </Nav>
