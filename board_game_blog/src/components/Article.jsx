3efc5da398ef070d64c9f7a4a32f8f8de32006e0
AndreaBar94 <andreabarocchi@gmail.com>
2023-07-10 11:47:33 +0200

    moved svgs in a different folder (cleaner code)

--- board_game_blog/src/components/Article.jsx
+++ board_game_blog/src/components/Article.jsx
@@ -10,6 +10,9 @@ import EditArticleModal from './EditArticleModal';
 import DeleteArticleModal from './DeleteArticleModal';
 import EditCommentModal from './EditCommentModal';
 import DeleteCommentModal from './DeleteCommentModal';
+import submitLogo from '../assets/svgs/submitLogo.svg';
+import editLogo from '../assets/svgs/editLogo.svg';
+import deleteLogo from '../assets/svgs/deleteLogo.svg';
 
 const Article = () => {
 
@@ -219,16 +222,11 @@ useEffect(() => {
           <Container className='d-flex justify-content-between p-0'>
             <Button onClick={handleArticleEdit} className='actionButton'>
               Edit Article
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-pencil ms-2" viewBox="0 0 16 16">
-                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-              </svg>
+              <img src={editLogo} alt="edit-logo" className='ms-2' />
             </Button>
             <Button onClick={handleArticleDelete} className="bg-danger border-danger">
               Delete Article
-              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-trash ms-2" viewBox="0 0 16 16">
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-              </svg>
+              <img src={deleteLogo} alt="delete-logo" className='ms-2' />
             </Button>
           </Container>
           </>
@@ -248,10 +246,7 @@ useEffect(() => {
         </Form.Group>
         <Button className='actionButton mt-2' onClick={handleCommentSubmit}>
           Submit Comment
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-chat-left-text ms-2" viewBox="0 0 16 16">
-            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-            <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-          </svg>
+          <img src={submitLogo} alt="submit-logo" className='ms-2' />
         </Button>
         {/* COMMENT MAP SECTION */}
         {comments.map((comment) => (
@@ -262,16 +257,11 @@ useEffect(() => {
                 <div className='d-flex justify-content-between'>
                   <Button onClick={() => handleEditComment(comment)} className='actionButton'>
                     Edit
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-pencil ms-2" viewBox="0 0 16 16">
-                      <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                    </svg>
+                    <img src={editLogo} alt="edit-logo" className='ms-2' />
                   </Button>
                   <Button onClick={() => handleCommentDelete(comment.commentId)} className="bg-danger border-danger align-items-center">
                     Delete
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-trash ms-2" viewBox="0 0 16 16">
-                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                    </svg>
+                    <img src={deleteLogo} alt="delete-logo" className='ms-2' />
                   </Button>
                 </div>
               )}
--- board_game_blog/src/components/PageNavbar.jsx
+++ board_game_blog/src/components/PageNavbar.jsx
@@ -4,6 +4,9 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/imgs/logo.png';
+import profileLogo from '../assets/svgs/profileLogo.svg';
+import homeLogo from '../assets/svgs/homeLogo.svg';
+import searchLogo from '../assets/svgs/searchLogo.svg';
 import { Button, Form } from 'react-bootstrap';
 import { searchArticleWithFilter } from '../redux/actions';
 import { useDispatch, useSelector } from 'react-redux';
@@ -69,9 +72,7 @@ const PageNavbar = () => {
                   <span>
                     Home
                   </span>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-house mx-2" viewBox="0 0 16 16">
-                    <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
-                  </svg>
+                  <img src={homeLogo} alt="home-logo" className='ms-2' />
                 </Link>
                 <Form className="d-column d-lg-flex align-items-center mx-5" onSubmit={handleSearchSubmit}>
                   <Form.Select className="my-2 me-2" value={selectedFilter} onChange={handleFilterChange}>
@@ -91,16 +92,19 @@ const PageNavbar = () => {
                     />
                   <Button variant="dark" type="submit" className='d-flex align-items-center'>
                     Search
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search ms-2" viewBox="0 0 16 16">
-                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                    </svg>
+                    <img src={searchLogo} alt="search-logo" className='ms-2' />
                   </Button>
                 </Form>
               </Nav>
               <div className='p-0 my-2 text-center'>
                 <Link className="text-decoration-none text-dark profileBtn" to="/profile">
                   <span className='d-none d-lg-inline'>Welcome back, {currentUser && currentUser.username}</span>
-                  <img src={currentUser && currentUser.profileImgUrl} alt="profileImg" width="40px" height="40px" className='object-fit-contain rounded-circle ms-1 border border-dark'/>
+                  <img 
+                    src={currentUser && currentUser.profileImgUrl ? currentUser.profileImgUrl : profileLogo}
+                    alt="profileImg" 
+                    width="40px" 
+                    height="40px" 
+                    className='object-fit-contain rounded-circle ms-1 border border-secondary profileImg'/>
                 </Link>
               </div>
             </Container>
